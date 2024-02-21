61ba08a4333b32aadb27fe1c413a2ce02f432325
Braulio <65780720+brauliotegui@users.noreply.github.com>
2024-02-21 15:14:52 +0100

    Update dbt_mapped_tracks.sql
    correct function to snowflake syntax

--- models/rudder/dbt_mapped_tracks.sql
+++ models/rudder/dbt_mapped_tracks.sql
@@ -7,7 +7,7 @@
 {{ config(materialized='table') }}
 
 select *
-        ,timestamp_diff(cast(timestamp as timestamp), cast(lag(timestamp) over(partition by dbt_visitor_id order by timestamp) as timestamp), minute) as idle_time_minutes
+        ,DATEDIFF('MINUTE', LAG("TIMESTAMP") OVER (PARTITION BY DBT_VISITOR_ID ORDER BY "TIMESTAMP"), "TIMESTAMP") AS IDLE_TIME_MINUTES
       from (
         select t.id as event_id
           ,t.anonymous_id
