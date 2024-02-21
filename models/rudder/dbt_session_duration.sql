6e3ce604cc9e544908d9cab792fc0426f6544c04
Braulio <65780720+brauliotegui@users.noreply.github.com>
2024-02-21 18:16:03 +0100

    Update dbt_session_duration.sql
    adapt to snowflake syntax for time diff

--- models/rudder/dbt_session_duration.sql
+++ models/rudder/dbt_session_duration.sql
@@ -9,7 +9,7 @@ Table containing a useful session metric - session duration
 select 
     s1.dbt_visitor_id
     , s1.session_id
-    , timestamp_diff(cast(s2.ended_at as timestamp), cast(s1.session_start_at as timestamp), minute) as session_duration
+    , DATEDIFF('MINUTE', CAST(S1.SESSION_START_AT AS TIMESTAMP), CAST(S2.ENDED_AT AS TIMESTAMP)) as SESSION_DURATION
 from
     {{ ref('dbt_session_tracks')}} as s1
     LEFT JOIN {{ ref('dbt_session_track_facts') }} as s2
